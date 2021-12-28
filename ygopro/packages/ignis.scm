f6c3d512092ce0c33ec8057d19eef636dd3d98c2
LordYuuma <lordyuuma@gmail.com>
2021-12-28 21:50:52 +0100

    ignis: Update nightlies.
    * ygopro/packages/ignis.scm (ignis-database-nightly): Update to 59e9199966daea2ce3531a4526cee3e1ab91348a.
    (ignis-scripts-nightly): Update to 6f4308ba305e17a2a6b7044b8619bcdb01115af1.

--- ygopro/packages/ignis.scm
+++ ygopro/packages/ignis.scm
@@ -355,9 +355,9 @@ built on top of that.")
    (license #f)))
 
 (define-public ignis-database-nightly
-  (let ((database-night "20211226")
-        (database-commit "8ff0e2b647a970f6ac0c30ea2f66c12bea1aa413")
-        (database-hash "1m8s8qy4p6pk4y2xb1v5jqi0vqd0kpgl0rpyh5jd2kgp71v1a28h")
+  (let ((database-night "20211228")
+        (database-commit "59e9199966daea2ce3531a4526cee3e1ab91348a")
+        (database-hash "18halzyjnb2wg9f4i653v8nmjld32jbvyb15zwzvk32cpswyamsb")
         (lflists-commit "27d9af0274122706c5550a7100eaffacd121ac08")
         (lflists-hash "0qyh16ipafv91yiiimzh52v3x3wk54qyj9rc74nadpyc3k1shr1j"))
     (package
@@ -469,9 +469,9 @@ built on top of that.")
     (license license:agpl3+)))
 
 (define-public ignis-scripts-nightly
-  (let ((scripts-night "20211226")
-        (scripts-commit "8c5a79103629bfb273df31d45a040840c9c4ab5e")
-        (scripts-hash "0dw04qjb6qy1jfw41664417hbl3mdq8jvwq1rlilgzw36qgd9ih3"))
+  (let ((scripts-night "20211228")
+        (scripts-commit "6f4308ba305e17a2a6b7044b8619bcdb01115af1")
+        (scripts-hash "1ymrd3m4a7bhwpsaw2dwqv94pdrlqyn8kkxd1lns09vzk4qcd008"))
     (package
       (inherit ignis-scripts-baseline)
       (version (string-append scripts-night "-nightly"))
